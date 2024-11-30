317dd13f6f10c867b8e3ba79c43b70c413cbc904
Indureka V.A <induarulprakasam94@gmail.com>
2024-11-30 21:54:02 +0530

    updated file

--- src/config/db.js
+++ src/config/db.js
@@ -11,7 +11,13 @@ const connectDB = async () => {
       }
   
 
-    await mongoose.connect(process.env.MONGO_URL);
+    
+
+
+    console.log("Connecting to MongoDB with URL:", process.env.MONGO_URL);
+
+  await mongoose.connect(process.env.MONGO_URL);
+
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
